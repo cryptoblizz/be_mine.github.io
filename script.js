33d45b1223d725302b65cf9dd39ad6777ac13f09
Vasu Goyal <vasugoyal12@gmail.com>
2025-02-14 00:37:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ document.getElementById('valentineButton').addEventListener('click', function() 
         response = confirm('Will you be my Valentine, Niyati Malik?');
         if (response) {
             alert('I am so glad!');
+            addFlowers();
         } else {
             alert('Sorry, not allowed. Please choose again.');
         }
     } while (!response);
 });
+
+function addFlowers() {
+    const container = document.querySelector('.container');
+    const flowerImg = document.createElement('img');
+    flowerImg.src = './flowers.png'; // Relative link to the local flower image
+    flowerImg.alt = 'Flower';
+    flowerImg.className = 'flower';
+    container.appendChild(flowerImg);
+}
